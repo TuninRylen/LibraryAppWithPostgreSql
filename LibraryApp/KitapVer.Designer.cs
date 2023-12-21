4dd505516d677fdbb5d93186cebaba9eb3e6658d
TuninRylen <132095456+TuninRylen@users.noreply.github.com>
2023-12-21 20:37:26 +0300

    v2

---
+++ LibraryApp/KitapVer.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace LibraryApp
+{
+    partial class KitapVer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(57, 215);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(524, 39);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(146, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(348, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Telefon Numarası Ara";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(774, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kitap İsmi Ara";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.ItemHeight = 31;
+            this.comboBox2.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "V",
+            "D",
+            "E",
+            "E",
+            "E",
+            "E",
+            "E",
+            "E",
+            "EE",
+            "EE",
+            "E",
+            "E",
+            "A",
+            "B",
+            "V",
+            "D",
+            "E",
+            "E",
+            "E",
+            "E",
+            "E",
+            "E",
+            "EE",
+            "EE",
+            "E",
+            "E",
+            "E",
+            "E",
+            "A",
+            "B",
+            "V",
+            "D",
+            "E",
+            "E",
+            "E",
+            "E",
+            "E",
+            "E",
+            "EE",
+            "EE",
+            "E",
+            "E",
+            "E",
+            "E",
+            "E",
+            "E"});
+            this.comboBox2.Location = new System.Drawing.Point(623, 215);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(524, 39);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(1184, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 93);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Kitabı Ver";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // KitapVer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1380, 760);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "KitapVer";
+            this.Text = "KitapVer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
